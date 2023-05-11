b3f4212be43b2c26b5ce053167e93ac7e4c38da6
VovaKyrychuk <kyrychuk81@gmail.com>
2023-05-11 21:28:39 +0300

    update App.jsx ImageGallery.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { Searchbar } from './Searchbar/Searchbar';
 import { Layout } from './Layout/Layout';
 import ImageGallery from './ImageGallery/ImageGallery';
 
-export default class App extends Component {
-  state = {
-    textSearch: '',
+export default function App() {
+  const [textSearch, setTextSearch] = useState('');
+
+  const handleSubmit = textSearch => {
+    setTextSearch(textSearch);
   };
 
-  handleSubmit = textSearch => {
-    this.setState({ textSearch });
-  };
-
-  render() {
-    const { textSearch } = this.state;
-
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        <Layout>
-          <ImageGallery value={textSearch} />
-        </Layout>
-
-        <GlobalStyle />
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      <Layout>
+        <ImageGallery value={textSearch} />
+      </Layout>
+      <GlobalStyle />
+    </>
+  );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import getImages from 'services/getApi';
 import { List } from './ImageGallery.styled';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
@@ -14,94 +14,81 @@ const Status = {
   REJECTED: 'rejected',
 };
 
-export default class ImageGallery extends Component {
-  static propTypes = {
-    value: PropTypes.string.isRequired,
-  };
+export default function ImageGallery({ value }) {
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [isShowModal, setIsShowModal] = useState(false);
+  const [modalData, setModalData] = useState({ tags: '' });
 
-  state = {
-    value: '',
-    images: [],
-    error: null,
-    status: Status.IDLE,
-    page: 1,
-    totalPages: 0,
-    isShowModal: false,
-    modalData: { tags: '' },
-  };
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (prevState.value !== nextProps.value) {
-      return { page: 1, value: nextProps.value };
-    }
-    return null;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { page } = this.state;
-    const prevValue = prevProps.value;
-    const nextValue = this.props.value;
-
-    if (prevValue !== nextValue || prevState.page !== page) {
-      this.setState({ status: Status.PENDING });
-
-      if (this.state.error) {
-        this.setState({ error: null });
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const images = await getImages.getImages(value, page);
+        setImages(prevImages =>
+          page === 1 ? images.hits : [...prevImages, ...images.hits]
+        );
+        setStatus(Status.RESOLVED);
+        setTotalPages(Math.floor(images.totalHits / 12));
+      } catch (error) {
+        setError(error);
+        setStatus(Status.REJECTED);
       }
-      getImages
-        .getImages(nextValue, page)
-        .then(images => {
-          this.setState(prevState => ({
-            images:
-              page === 1 ? images.hits : [...prevState.images, ...images.hits],
-            status: Status.RESOLVED,
-            totalPages: Math.floor(images.totalHits / 12),
-          }));
-        })
-        .catch(error => this.setState({ error, status: Status.REJECTED }));
+    };
+
+    if (value) {
+      setStatus(Status.PENDING);
+      if (error) {
+        setError(null);
+      }
+      fetchImages();
     }
+  }, [value, page, error]);
+
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleModalClose = () => {
+    setIsShowModal(false);
+  };
+
+  const handleImageClick = modalData => {
+    setModalData(modalData);
+    setIsShowModal(true);
+  };
+
+  if (status === Status.PENDING) {
+    return <Loader />;
   }
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  setModalData = modalData => {
-    this.setState({ modalData, isShowModal: true });
-  };
-
-  handleModalClose = () => {
-    this.setState({ isShowModal: false });
-  };
-
-  render() {
-    const { images, status, page, totalPages, isShowModal, modalData } =
-      this.state;
-
-    if (status === 'pending') {
-      return <Loader />;
-    }
-
-    if (status === 'resolved') {
-      return (
-        <>
-          <List>
-            {images.map(image => (
-              <ImageGalleryItem
-                key={image.id}
-                item={image}
-                onImageClick={this.setModalData}
-              />
-            ))}
-          </List>
-          {images.length > 0 && status !== 'pending' && page <= totalPages && (
-            <Button onClick={this.handleLoadMore}>Load More</Button>
-          )}
-          {isShowModal && (
-            <Modal modalData={modalData} onModalClose={this.handleModalClose} />
-          )}
-        </>
-      );
-    }
+  if (status === Status.RESOLVED) {
+    return (
+      <>
+        <List>
+          {images.map(image => (
+            <ImageGalleryItem
+              key={image.id}
+              item={image}
+              onImageClick={handleImageClick}
+            />
+          ))}
+        </List>
+        {images.length > 0 && page <= totalPages && (
+          <Button onClick={handleLoadMore}>Load More</Button>
+        )}
+        {isShowModal && (
+          <Modal modalData={modalData} onModalClose={handleModalClose} />
+        )}
+      </>
+    );
   }
+
+  return null;
 }
+
+ImageGallery.propTypes = {
+  value: PropTypes.string.isRequired,
+};
